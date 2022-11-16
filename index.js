fb4af62c096fb7672fa921f31a786f772c5d1f49
xackerboydili <isoqovdilshod47@gmail.com>
2022-11-16 19:14:16 +0500

    20 array exercises

---
+++ index.js
@@ -0,0 +1,38 @@
+'1'
+// function getAverage(marks){
+//     return Math.floor(marks.reduce((a, b) => a + b) / marks.length)
+// }
+// console.log(getAverage())
+
+'2'
+// function monkeyCount(n) {
+//     return Array.from({length: n}, (a, index) => index + 1)
+// }
+// console.log(monkeyCount())
+
+'3'
+
+// const flip=(d, a)=>{
+//     return d == 'R' ? a.sort((a, b) => a-b) : a.sort((a, b) => b-a)
+// }
+
+'4'
+
+// function duckDuckGoose(players, goose) {
+//     return players[(goose - 1) % players.length].name
+// }
+
+'5'
+
+// function points(games) {
+//     return games.reduce((a, [b, c, d]) => a + (b > d ? 3 : b == d), 0)
+// }
+
+'6'
+
+// function howMuchILoveYou(nbPetals) {
+//     return ['not at all', 'I love you', 'a little', 'a lot', 'passionately', 'madly'][nbPetals % 6]
+// }
+
+'7'
+
